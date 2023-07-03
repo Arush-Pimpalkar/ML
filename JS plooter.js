0fee54d62ab364dceaf13554a21ee1994219724a
Arush-Pimpalkar <arush.pimpalkar@gmail.com>
2023-07-03 17:47:11 +0530

    test

--- JS plooter.js
+++ JS plooter.js
@@ -3,38 +3,57 @@ var xMin = -100;
 var xMax = 100;
 var yMin = -100;
 var yMax = 100;
-
-console.log("heheheh"); 
-
+let bias = 1;
 let colorPointBlackX = [];
 let colorPointBlackY = [];
 let colorPointOrangeX = [];
 let colorPointOrangeY = [];
 const xValues = [];
 const yValues = [];
+// Compute Random Weights
+let xWeights = [];
+let yWeights = [];
 
 //making dataset array
-for (let x = xMin; x < xMax;) {
+for (let x = xMin; x < xMax; ) {
   xValues.push((xMax - xMin) * Math.random() + xMin);
   x = x + 0.5;
 }
-for (let y = yMin; y < yMax;) {
+for (let y = yMin; y < yMax; ) {
   yValues.push((yMax - yMin) * Math.random() + yMin);
   y = y + 0.5;
 }
+
+function rWeights(array, xCoor) {
+  for (let i = 0; i < xCoor; i++) {
+    array[i] = Math.random() * 2 - 1; // range : -1 to +1
+  }
+}
+rWeights(xWeights, xValues.length);
+rWeights(yWeights, yValues.length);
+console.log(xWeights, yWeights);
+
 function graph(x) {
   return 3 * x + 0.75;
 }
 function desired(y, x) {
-  return y - graph(x);
+  if (y - graph(x) > 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+function activate(inputs, weights) {
+  let sum = 0;
+  sum += inputs[i] * weights[i];  // multiplies the weights and coors
+  return sum;
 }
 
 
-const ptron = new Perceptron(2, 0.0005);
 
-ptron.train(xValues[i], yValues[i], desired(yValues[i], xValues[i]))
 
-/*
+
+
 for (let i = 0; i < xValues.length; i++)
   if (3 * xValues[i] + 0.75 - yValues[i] > 0) {
     colorPointBlackX.push(xValues[i]);
@@ -43,7 +62,7 @@ for (let i = 0; i < xValues.length; i++)
     colorPointOrangeX.push(xValues[i]);
     colorPointOrangeY.push(yValues[i]);
   }
-*/
+
 var yVal2 = [];
 xValues.forEach((element) => {
   yVal2.push(graph(element));
@@ -71,4 +90,4 @@ const blackPoints = {
 };
 
 var data = [lines, blackPoints, orangePoints];
-Plotly.newPlot("myPlot", data);
+//Plotly.newPlot("myPlot", data);
